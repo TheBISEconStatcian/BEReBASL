bbd83b0fec5cddd5cb7910580af08f50c0e21d5a
Mateo Ordonez Serna <econstats001@gmail.com>
2026-02-12 17:07:08 +0100

    some taste updates to the function

--- berebasl/simulation/acceptance_loop.py
+++ berebasl/simulation/acceptance_loop.py
@@ -12,22 +12,20 @@ def accept_based_on_top_percentent_of_arbitrary_var(
         raise ValueError("var_for_rule outside of index")
     
     cutoff = torch.quantile(features[:, var_for_rule], 1 - top_percent)
-
     accepts = features[:, var_for_rule] >= cutoff
-
 
     count_defaults_within_accepts = (default_flag[accepts] == default_value).sum()
     if count_defaults_within_accepts < min_count_bads:
-        lidx_defaults_non_accepted = (~accepts) & (default_flag == default_value)
-        defaults_still_selectable = lidx_defaults_non_accepted.sum()
+        mask_defaults_non_accepted = (~accepts) & (default_flag == default_value)
+        defaults_still_selectable = mask_defaults_non_accepted.sum()
         if defaults_still_selectable == 0:
             return accepts
         
         count_bads_to_still_achieve = min_count_bads - count_defaults_within_accepts
-        var_for_rule_vals_of_rejected_defaults = features[lidx_defaults_non_accepted][:, var_for_rule]
+        var_for_rule_vals_of_rejected_defaults = features[mask_defaults_non_accepted][:, var_for_rule]
         
         if defaults_still_selectable <= count_bads_to_still_achieve:
-            var_for_rule_vals_of_rejected_defaults = features[lidx_defaults_non_accepted][:, var_for_rule]
+            var_for_rule_vals_of_rejected_defaults = features[mask_defaults_non_accepted][:, var_for_rule]
             accept_rule_to_include_all_defaults = features[:, var_for_rule] >=  var_for_rule_vals_of_rejected_defaults.min()
             return accept_rule_to_include_all_defaults
         
