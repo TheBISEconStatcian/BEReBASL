53e68021d9e3d8fd10f84ac2b20590783d9bcec5
Mateo Ordonez Serna <econstats001@gmail.com>
2026-02-21 17:33:42 +0100

    beginning assert function

--- berebasl/utils/norm_shape_handles.py
+++ berebasl/utils/norm_shape_handles.py
@@ -1,2 +1,13 @@
+from typing import Literal, Optional
+
 import torch
 
+
+def assert_tensors(
+        *tensors, 
+        tensor_names : Optional[str],
+        checks : List[]
+    ):
+    if len(tensors) == 0:
+        return
+    pass
